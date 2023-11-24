01574001873a4244f08c536f9c444a63d0377d2a
Đoàn Đức Anh <141485642+Syjoon@users.noreply.github.com>
2023-11-24 19:53:50 +0700

    Update memcpy().c

--- APIs/Code/memcpy().c
+++ APIs/Code/memcpy().c
@@ -1,11 +1,33 @@
-#include<stdio.h>
-#include<string.h>
+// Đoạn code C minh họa cho lỗi tràn bộ đệm khi sử dụng hàm memcpy()
+
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
-  char str[50] = "Hello World!";
-  char copy_str[50];
-  memcpy(copy_str, str, strlen(str) + 1);
-  printf("Copy string: %s", copy_str);
-  return 0;
+    char dest[10]; // mảng ký tự đích có kích thước 10
+    char src[] = "Hello, world!"; // chuỗi ký tự nguồn có kích thước 14 (bao gồm ký tự kết thúc '\0')
+    int x = 100; // một biến số nguyên
+
+    printf("Before memcpy:\n");
+    printf("dest = %s\n", dest); // in ra giá trị của mảng đích
+    printf("src = %s\n", src); // in ra giá trị của chuỗi nguồn
+    printf("x = %d\n", x); // in ra giá trị của biến x
+
+    memcpy(dest, src, strlen(src) + 1); // sao chép chuỗi nguồn vào mảng đích, bao gồm cả ký tự kết thúc '\0'
+
+    printf("After memcpy:\n");
+    printf("dest = %s\n", dest); // in ra giá trị của mảng đích
+    printf("src = %s\n", src); // in ra giá trị của chuỗi nguồn
+    printf("x = %d\n", x); // in ra giá trị của biến x
+
+    return 0;
 }
+//Before memcpy:
+//dest = @
+//src = Hello, world!
+//x = 100
+//After memcpy:
+//dest = Hello, world!
+//src = Hello, world!
+//x = 2188396
