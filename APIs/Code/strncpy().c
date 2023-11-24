cfc6a2f2cfae50997a48579896ad688ca0b15c36
Đoàn Đức Anh <141485642+Syjoon@users.noreply.github.com>
2023-11-24 20:08:28 +0700

    Update strncpy().c

--- APIs/Code/strncpy().c
+++ APIs/Code/strncpy().c
@@ -21,7 +21,7 @@ int main()
     printf("src = %s\n", src); // in ra giá trị của chuỗi nguồn
     printf("x = %d\n", x); // in ra giá trị của biến x
 
-    strncpy(dest, src, 10); // sao chép 10 ký tự từ chuỗi nguồn vào mảng đích
+    strncpy(dest, src, 15); // sao chép 10 ký tự từ chuỗi nguồn vào mảng đích
 
     printf("After strncpy:\n");
     printf("dest = %s\n", dest); // in ra giá trị của mảng đích
@@ -33,9 +33,8 @@ int main()
 
 
 
-// Lỗi tràn bộ đệm: khi bạn sao chép 10 ký tự từ chuỗi nguồn vào mảng đích, hàm strncpy() đã ghi đè lên vùng nhớ của biến x, dẫn đến việc giá trị của biến x bị thay đổi từ 100 thành 33. Nếu bạn sử dụng một trình biên dịch khác, hoặc thay đổi vị trí của các biến, bạn có thể nhận được kết quả khác nhau, nhưng đều cho thấy sự sai lệch của dữ liệu.
+// Lỗi tràn bộ đệm: khi bạn sao chép 10 ký tự từ chuỗi nguồn vào mảng đích, hàm strncpy() đã ghi đè lên vùng nhớ của biến x, dẫn đến việc giá trị của biến x bị thay đổi từ 100 thành 2188396. Nếu bạn sử dụng một trình biên dịch khác, hoặc thay đổi vị trí của các biến, bạn có thể nhận được kết quả khác nhau, nhưng đều cho thấy sự sai lệch của dữ liệu.
 // Lỗi thiếu ký tự kết thúc chuỗi: khi bạn sao chép 10 ký tự từ chuỗi nguồn vào mảng đích, hàm strncpy() đã không thêm ký tự kết thúc chuỗi ('\0') vào cuối mảng đích, dẫn đến việc mảng đích không được định dạng đúng, gây ra các lỗi khi in ra màn hình. Bạn có thể thấy rằng mảng đích không chỉ chứa 10 ký tự "Hello, worl", mà còn có thêm các ký tự rác không mong muốn.
 
-//Để tránh các lỗi khi sử dụng hàm strncpy(), bạn cần chắc chắn rằng kích thước của vùng nhớ đích lớn hơn kích thước của vùng nhớ nguồn, và thêm ký tự kết thúc chuỗi ('\0') vào cuối vùng nhớ đích nếu cần. Bạn cũng nên kiểm tra giới hạn của các tham số truyền vào hàm strncpy(), và sử dụng các hàm an toàn hơn như strlcpy() hoặc strncpy_s() .
 
 
