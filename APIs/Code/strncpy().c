2b908b8add251b442fa8b198e7e83f8908db79ce
Đoàn Đức Anh <141485642+Syjoon@users.noreply.github.com>
2023-11-24 20:01:16 +0700

    Update strncpy().c

--- APIs/Code/strncpy().c
+++ APIs/Code/strncpy().c
@@ -1,11 +1,41 @@
-#include<stdio.h>
-#include<string.h>
+//Hàm strncpy() có thể gây ra các lỗi như:
+//Lỗi tràn bộ đệm (buffer overflow): khi bạn sao chép quá nhiều ký tự vào một chuỗi đích có kích thước nhỏ hơn, dẫn đến việc ghi đè lên các vùng nhớ liền kề, gây ra các hậu quả không mong muốn như thay đổi giá trị của các biến, làm hỏng các con trỏ, hoặc thậm chí là thực thi mã độc¹².
+//Lỗi thiếu ký tự kết thúc chuỗi (null terminator): khi  sao chép một số ký tự bằng hoặc lớn hơn kích thước của chuỗi đích, 
+// hàm strncpy() sẽ không tự động thêm ký tự kết thúc chuỗi ('\0') vào cuối chuỗi đích, dẫn đến việc chuỗi đích không được định dạng đúng
+//gây ra các lỗi khi sử dụng các hàm xử lý chuỗi khác³⁴.
+
+// Đoạn code C minh họa cho các lỗi khi sử dụng hàm strncpy()
+
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
-  char str[50] = "Hello World!";
-  char copy_str[50];
-  strncpy(copy_str, str, 8);
-  printf("Copy string: %s", copy_str);
-  return 0;
+    char dest[10]; // mảng ký tự đích có kích thước 10
+    char src[] = "Hello, world!"; // chuỗi ký tự nguồn có kích thước 14 (bao gồm ký tự kết thúc '\0')
+    int x = 100; // một biến số nguyên
+
+    printf("Before strncpy:\n");
+    printf("dest = %s\n", dest); // in ra giá trị của mảng đích
+    printf("src = %s\n", src); // in ra giá trị của chuỗi nguồn
+    printf("x = %d\n", x); // in ra giá trị của biến x
+
+    strncpy(dest, src, 10); // sao chép 10 ký tự từ chuỗi nguồn vào mảng đích
+
+    printf("After strncpy:\n");
+    printf("dest = %s\n", dest); // in ra giá trị của mảng đích
+    printf("src = %s\n", src); // in ra giá trị của chuỗi nguồn
+    printf("x = %d\n", x); // in ra giá trị của biến x
+
+    return 0;
 }
+```
+
+
+
+// Lỗi tràn bộ đệm: khi bạn sao chép 10 ký tự từ chuỗi nguồn vào mảng đích, hàm strncpy() đã ghi đè lên vùng nhớ của biến x, dẫn đến việc giá trị của biến x bị thay đổi từ 100 thành 33. Nếu bạn sử dụng một trình biên dịch khác, hoặc thay đổi vị trí của các biến, bạn có thể nhận được kết quả khác nhau, nhưng đều cho thấy sự sai lệch của dữ liệu.
+// Lỗi thiếu ký tự kết thúc chuỗi: khi bạn sao chép 10 ký tự từ chuỗi nguồn vào mảng đích, hàm strncpy() đã không thêm ký tự kết thúc chuỗi ('\0') vào cuối mảng đích, dẫn đến việc mảng đích không được định dạng đúng, gây ra các lỗi khi in ra màn hình. Bạn có thể thấy rằng mảng đích không chỉ chứa 10 ký tự "Hello, worl", mà còn có thêm các ký tự rác không mong muốn.
+
+//Để tránh các lỗi khi sử dụng hàm strncpy(), bạn cần chắc chắn rằng kích thước của vùng nhớ đích lớn hơn kích thước của vùng nhớ nguồn, và thêm ký tự kết thúc chuỗi ('\0') vào cuối vùng nhớ đích nếu cần. Bạn cũng nên kiểm tra giới hạn của các tham số truyền vào hàm strncpy(), và sử dụng các hàm an toàn hơn như strlcpy() hoặc strncpy_s()⁵ .
+
+
