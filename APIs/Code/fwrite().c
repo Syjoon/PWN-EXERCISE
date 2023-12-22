bb96a95b0fbb736258e1c8485e96d792202dd1aa
Đoàn Đức Anh <141485642+Syjoon@users.noreply.github.com>
2023-12-22 20:58:30 +0700

    Create fwrite().c

---
+++ APIs/Code/fwrite().c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main () {
+   FILE *fp;
+   char str[] = "C programming tutorial.";
+   fp = fopen( "test.txt" , "w" );
+   fwrite(str , 1 , sizeof(str) , fp );
+
+   fclose(fp);
+   fp = fopen("test.txt","r");
+   int c;
+   while(1) {
+      c = fgetc(fp);
+      if( feof(fp) ) {
+         break ;
+      }
+      printf("%c", c);
+   }
+   fclose(fp);
+   return(0);
+}
+
+//Output: C programming tutorial.
