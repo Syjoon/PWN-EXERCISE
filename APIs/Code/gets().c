9bff9801ff5c758280cd15b1a2c062d41489fae8
Đoàn Đức Anh <141485642+Syjoon@users.noreply.github.com>
2023-12-22 20:48:52 +0700

    Update gets().c

--- APIs/Code/gets().c
+++ APIs/Code/gets().c
@@ -5,3 +5,5 @@ gets(str);
 printf("%s", str);
 return;
 }
+//Mã trông đơn giản, nó đọc chuỗi từ đầu vào tiêu chuẩn và in chuỗi đã nhập, nhưng nó bị Tràn bộ đệm vì gets() không thực hiện bất kỳ thử nghiệm giới hạn mảng nào.
+//gets() tiếp tục đọc cho đến khi nhìn thấy ký tự dòng mới. 
