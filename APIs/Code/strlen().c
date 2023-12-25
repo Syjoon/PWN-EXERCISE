cd7b871427dc3bf28c84b16b7cf9b69ef80f75df
Đoàn Đức Anh <141485642+Syjoon@users.noreply.github.com>
2023-12-25 20:26:50 +0700

    Update strlen().c

--- APIs/Code/strlen().c
+++ APIs/Code/strlen().c
@@ -8,3 +8,16 @@ int main()
   printf("Size of string: %d", length);
   return 0;
 }
+
+/////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char *str = NULL; // con trỏ NULL không trỏ đến một chuỗi ký tự hợp lệ
+    int len = strlen(str); // gọi hàm strlen() với tham số không hợp lệ
+    printf("Độ dài của chuỗi là %d\n", len); // in ra kết quả không xác định
+    return 0;
+}
+
