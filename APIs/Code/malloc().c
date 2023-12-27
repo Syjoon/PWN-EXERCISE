7faa2523784412073b6ee40237df76147d2c190e
Đoàn Đức Anh <141485642+Syjoon@users.noreply.github.com>
2023-12-27 22:12:12 +0700

    Update malloc().c

--- APIs/Code/malloc().c
+++ APIs/Code/malloc().c
@@ -19,3 +19,15 @@ int main()
 	free(ptr);
 	return 0;
 }
+////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *ptr = malloc(10); // Cấp phát 10 byte
+    ptr[0] = 1; // Gán giá trị cho phần tử đầu tiên
+    ptr[9] = 2; // Gán giá trị cho phần tử thứ 10
+    printf("%d %d\n", ptr[0], ptr[9]); // In ra giá trị của hai phần tử
+    free(ptr); // Giải phóng bộ nhớ
+    return 0;
+}
